0a640dc5cc22b48ec2822e385cd41f2e87667fd0
Antonio Cantero Ruiz <acanteroruiz.dev@gmail.com>
2024-10-30 08:44:34 +0100

    Add shell script for running tests with coverage

---
+++ run_test.sh
@@ -0,0 +1,15 @@
+# Main script
+echo "Running tests...."
+fvm flutter test --coverage --test-randomize-ordering-seed random
+echo "Tests completed...."
+echo "Filtering coverage report...."
+lcov --remove coverage/lcov.info \
+'*/view/*' \
+'*/pages/*' \
+'*/widgets/*' \
+'*.g.dart' \
+-o coverage/lcov_filtered.info
+echo "Generating coverage report...."
+genhtml coverage/lcov_filtered.info -o coverage/
+echo "Coverage report generated...."
+open coverage/index.html
